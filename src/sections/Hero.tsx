fbd58e28d7f64c0f7ce4f803086acf9e1561ab15
Bryan <bryandang10@gmail.com>
2024-06-29 23:00:50 -0700

    responsive pfp

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,11 +2,15 @@ import { SectionWrapper } from "../components/SectionComponents";
 
 export default function Hero() {
   return (
-    <div className="pt-20 sm:pt-40 pb-40 sm:pb-60">
+    <div className="pt-10 sm:pt-20 pb-10 sm:pb-20">
       <SectionWrapper>
-        <p className="text-3xl sm:text-4xl font-extrabold">HI, I'M BRYAN</p>
-        <p className="text-base sm:text-xl">A Vancouver-based CS student that likes to work in web and game development</p>
-        {/* <img src="src/assets/profile.jpg" alt="Bryan profile picture" width={400} /> */}
+        <div className="flex flex-col sm:flex-row items-center">
+          <div className="flex-1 mr-20">
+            <p className="text-3xl sm:text-4xl font-extrabold">HI, I'M BRYAN</p>
+            <p className="text-base sm:text-xl">A Vancouver-based CS student that likes to work in web and game development</p>
+          </div>
+          <img src="src/assets/profile.jpg" alt="Bryan profile picture" width={350} />
+        </div>
       </SectionWrapper>
     </div>
   )
