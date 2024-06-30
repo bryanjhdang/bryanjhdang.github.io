eceaaa30b046bb835568fc33fb7bee7ae8c25dc6
Bryan <bryandang10@gmail.com>
2024-06-30 00:06:25 -0700

    move header to left

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,10 @@ export default function Header() {
   return (
     <div className="hidden sm:block pt-10 pb-10 sm:pb-20 flow-root">
       <SectionWrapper>
-        <a href="https://bryandang.substack.com/" target="_blank" className="text-base hover:underline float-right ml-10">Blog</a>
-        <p className="text-base hover:underline float-right ml-10">Projects</p>
-        <p className="text-base hover:underline float-right">About</p>
+        <p className="text-base hover:underline float-left mr-10 font-bold">Bryan Dang</p>
+        <p className="text-base hover:underline float-left mr-10">About</p>
+        <p className="text-base hover:underline float-left mr-10">Projects</p>
+        <a href="https://bryandang.substack.com/" target="_blank" className="text-base hover:underline float-left mr-10">Blog</a>
       </SectionWrapper>
     </div>
   )
