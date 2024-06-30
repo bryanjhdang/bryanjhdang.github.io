dfaed959c52e31bf4ad37a98cafac6b1ce3255ea
Bryan <bryandang10@gmail.com>
2024-06-30 10:54:30 -0700

    add some projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -24,14 +24,24 @@ export default function Projects() {
         <SectionTitle title="PROJECTS" />
 
         <div>
-          <p>Past Your Peak</p>
-          <p>A game made for the Mountain Top Game Jam 2023. It's a reverse rogue-like game where you lose powers as you progress through the stages, meant to represent how you 'peaked' as the theme. It ended up winning the Creativity Award ($200).</p>
           <div>
-            <p>Unity</p>
+
           </div>
           <div>
-            <p>View Project</p>
-            <p>Git Repo</p>
+            <div className="pb-5">
+              <p className="text-xl font-semibold pb-2">Past Your Peak</p>
+              <p className="text-base">A game made for the Mountain Top Game Jam 2023. It's a reverse rogue-like game where you lose powers as you progress through the stages, meant to represent how you 'peaked' as the theme. It ended up winning the Creativity Award ($200).</p>
+            </div>
+            <div className="pb-5">
+              <p className="text-base">Stuff used:</p>
+              <ul className="list-disc list-inside text-base">
+                <li>Unity</li>
+              </ul>
+            </div>
+            <div>
+              <a href="https://bryd.itch.io/past-your-peak" target="_blank" className="text-base text-blue-600 underline mr-5">View Project</a>
+              <a href="https://github.com/lucastmah/Mute_Jammers" target="_blank" className="text-base text-blue-600 underline mr-5">Git Repo</a>
+            </div>
           </div>
         </div>
 
