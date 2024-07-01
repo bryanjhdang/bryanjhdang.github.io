edff6608bd49a36432e2d27455ba5f1f92c9ab14
Bryan <bryandang10@gmail.com>
2024-06-30 19:45:00 -0700

    fix unused declaration

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,4 @@
 import { SectionWrapper } from "../components/SectionComponents";
-import EmailImg from "../assets/images/email-outline.png";
-import LinkedinImg from "../assets/images/linkedin-black.png";
 
 export default function Hero() {
   return (
@@ -8,14 +6,6 @@ export default function Hero() {
       <SectionWrapper>
         <p className="text-3xl xl:text-4xl font-extrabold">HI, I'M BRYAN</p>
         <p className="text-xl lg:text-2xl">A Vancouver CS student that likes to work in web and game development.</p>
-        {/* <div className="pt-8 flex flex-row gap-5 text-xl items-center">
-          <a href="mailto:bda37@sfu.ca" className="underline">
-            <img src={EmailImg} alt="Email" className="w-12" />
-          </a>
-          <a href="https://www.linkedin.com/in/bryanjhdang/" target="blank" className="underline">
-            <img src={LinkedinImg} alt="LinkedIn" className="w-9" />
-          </a>
-        </div> */}
       </SectionWrapper>
     </div>
   )
