750566f188b2ecafe7c5b0b38c62c6626fa3c1f5
unknown <bryandang10@gmail.com>
2024-09-02 13:02:35 -0700

    more scaling

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from './sections/Footer'
 function App() {
   return (
     <div className='flex flex-col items-center justify-center'>
-      <div className='w-full px-8 md:px-20 max-w-6xl 3xl:max-w-7xl'>
+      <div className='w-full px-8 md:px-20 max-w-5xl 3xl:max-w-7xl'>
 
         {/* px-10 sm:px-20 lg:px-40 xl:px-60 */}
         <Header />
